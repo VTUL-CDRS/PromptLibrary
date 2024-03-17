844c5eae40124de6329a690fdd975e53f56d41aa
gshin06 <shin.gabriel626@gmail.com>
2024-03-17 15:50:04 -0400

    Switched server code to TypeScript

--- src/api/users/route.ts
+++
@@ -1,7 +0,0 @@
-import { prisma } from '@/lib/prisma';
-
-export async function GET(request: Request) {
-    const users = await prisma.account.findMany()
-}
-
-export async function POST(request: Request) {}
