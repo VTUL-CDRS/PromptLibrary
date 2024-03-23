27bd0c2c5598360087b8f5a0f515dd342397f6f9
dshinjc <daniel.shin.0626@gmail.com>
2024-03-23 16:14:29 -0400

    Reorganizing again

---
+++ TempHold/index.ts
@@ -0,0 +1,12 @@
+import express, { Express, Request, Response } from "express";
+const port = 8000;
+
+const app: Express = express();
+
+app.get("/", (_: Request, res: Response) => {
+  res.send("HELLO");
+});
+
+app.listen(port, () => {
+  console.log(`now listening on port ${port}`);
+});
