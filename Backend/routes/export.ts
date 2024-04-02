253e220905051fdc2789d0768085d0eaf43d86de
gshin06 <shin.gabriel626@gmail.com>
2024-04-02 16:38:03 -0400

    Modified the export function a bit

--- Backend/routes/export.ts
+++ Backend/routes/export.ts
@@ -4,14 +4,19 @@ import express, { Request, Response } from "express";
 // Create the router object
 const router = express.Router();
 
-// Search for and return a JSON object of prompts with the id
+/** 
+ * Function to export a list of prompt ids. Takes a json array of ids separated by a +.
+ */
 router.get("/", async (req: Request, res: Response) => {
   try {
     const ids = req.query.ids;
     if (typeof ids === "string") {
+      // Convert to integer array
       const idArray = ids.split("+").map(function (item) {
         return parseInt(item, 10);
       });
+      
+      // Find prompts that match the ids provided
       const prompts = await prisma.prompt.findMany({
         where: {
           id: {
@@ -19,6 +24,8 @@ router.get("/", async (req: Request, res: Response) => {
           },
         },
       });
+
+      // Return a JSON
       res.json(prompts);
     } else {
       throw new Error("IDs must be provided as a plus-separated list");
