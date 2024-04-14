8b950c0d4659807353a220a6663accf6b1a382af
FutureTerminal <zyanw236@gmail.com>
2024-04-13 21:42:43 -0400

    ensure delete is working, merge attempt

---
+++ Backend/dist/llmSeed.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.llms = void 0;
+exports.llms = [
+    { llmName: "ChatGPT" },
+    { llmName: "Bard" },
+    { llmName: "CoPilot" }
+];
+//# sourceMappingURL=llmSeed.js.map
