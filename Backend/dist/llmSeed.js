4fb5274aade2b711538e74b872409d912a9f4440
dshinjc <daniel.shin.0626@gmail.com>
2024-04-16 10:04:18 -0400

    Export all

--- Backend/dist/llmSeed.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.llms = void 0;
-exports.llms = [
-    { llmName: "ChatGPT" },
-    { llmName: "Bard" },
-    { llmName: "CoPilot" }
-];
-//# sourceMappingURL=llmSeed.js.map
